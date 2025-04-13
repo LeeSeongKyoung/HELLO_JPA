82b2e947a59fe16591a650f7a13df5d4fb4874d8
lleess <llee_ss@naver.com>
2025-04-13 19:57:29 +0900

    first commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,61 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // EntityManagerFactory는 애플리케이션 로딩 시점에 딱 하나만 만들어야함
+        // persistenceUnitName은 persistence.xml에 설정한 name 값
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // entityManager 호출
+        // 한 일관적인 단위를 할 때마다 EntityManager 만들어야함
+        EntityManager em = emf.createEntityManager();
+
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 일어나야함
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // 호출 후 실제 code 작성
+        try {
+
+//            1. 회원 등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+
+//            2.회원 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//            em.remove(findMember);
+
+            // 3. 회원 수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            // 전체 회원 조회
+            // JPQL : 대상이 테이블이 아닌 entity 객체
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name: " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
