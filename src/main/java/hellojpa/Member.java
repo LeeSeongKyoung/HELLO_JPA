38555bb99577fceef38c10c6b8a5a7d601f05c83
lleess <llee_ss@naver.com>
2025-06-06 18:38:18 +0900

    기기본키 매핑 sequence

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,9 +10,10 @@ import java.util.Date;
 public class Member {
     // Entity = 데이터베이스 테이블과 매핑되는 클래스
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     // AUTO : DB 방언에 맞춰 자동 시퀀스 생성
     // mySql : auto_increment
+    // 참고 : 각 테이블마다 시퀀스 부여하고 싶다면 @SequenceGenerator 사용
     private Long id;
 
     // db 컬럼명 name과 객체 username 연결
